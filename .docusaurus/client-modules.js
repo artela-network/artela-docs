988251a1ce3f482b21994a8bdab6211831c5a655
zhanjun <zhanjun@artela.network>
2023-10-31 15:34:19 +0800

    Get started

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/admin/mytech/go-work/src/github.com/artela-network/artela-docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/admin/mytech/go-work/src/github.com/artela-network/artela-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/admin/mytech/go-work/src/github.com/artela-network/artela-docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/admin/mytech/go-work/src/github.com/artela-network/artela-docs/src/css/custom.css'),
-];
