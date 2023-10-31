6d273bc92d4eedd71a0260b335c21186f542796d
yuan <fatfishman90@gmail.com>
2023-10-31 15:30:04 +0800

    fix: remove .docusaurus.

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/jack/Projects/js/artela-docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/jack/Projects/js/artela-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/jack/Projects/js/artela-docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/jack/Projects/js/artela-docs/src/css/custom.css'),
-];
